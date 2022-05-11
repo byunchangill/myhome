b579fdf57ef46ab4f4b64affae9920ce3988c4ca
chang <91526196+BYUNCHANGILL@users.noreply.github.com>
2022-05-11 15:17:50 +0900

    layout 구성

---
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";
+    }
+}
